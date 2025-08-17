3d8183dd7e25fb83f9ef6890981e51a41844f421
Anh Tu <sweantu@gmail.com>
2025-08-17 18:50:47 +0700

    feat: add type function

--- backend/app/database/repositories/user.py
+++ backend/app/database/repositories/user.py
@@ -12,20 +12,20 @@ class UserRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    async def create(self, user: UserCreate):
+    async def create(self, user: UserCreate) -> User:
         db_user = User(name=user.name, email=user.email)
         self.db.add(db_user)
         await self.db.commit()
         await self.db.refresh(db_user)
         return db_user
 
-    async def get(self, user_id: uuid.UUID):
+    async def get(self, user_id: uuid.UUID) -> User:
         result = await self.db.execute(select(User).filter(User.id == user_id))
         user = result.scalars().first()
         if user is None:
             raise HTTPException(status_code=404, detail="User not found")
         return user
 
-    async def get_many(self, skip: int = 0, limit: int = 10):
+    async def get_many(self, skip: int = 0, limit: int = 10) -> list[User]:
         result = await self.db.execute(select(User).offset(skip).limit(limit))
-        return result.scalars().all()
+        return list(result.scalars().all())
--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 from pydantic import UUID4
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.schemas.user import UserCreate
+from app.api.schemas.user import UserCreate, UserResponse
 from app.core.database import DbDep
 from app.database.repositories.user import UserRepository
 
@@ -13,14 +13,15 @@ class UserService:
     def __init__(self, db: AsyncSession):
         self.user_repository = UserRepository(db)
 
-    async def create(self, user: UserCreate):
+    async def create(self, user: UserCreate) -> UserResponse:
         return await self.user_repository.create(user)
 
-    async def get(self, user_id: UUID4):
+    async def get(self, user_id: UUID4) -> UserResponse:
         return await self.user_repository.get(user_id)
 
-    async def get_many(self, skip: int = 0, limit: int = 10):
-        return await self.user_repository.get_many(skip, limit)
+    async def get_many(self, skip: int = 0, limit: int = 10) -> list[UserResponse]:
+        result = await self.user_repository.get_many(skip, limit)
+        return list(result)
 
 
 def get_user_service(db: DbDep) -> UserService:
