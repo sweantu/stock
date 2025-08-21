c4bb77599b1550cca329965045a46e8083e208d7
Anh Tu <sweantu@gmail.com>
2025-08-21 16:50:10 +0700

    feat: login vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoginForm from './LoginForm.vue';
+
+</script>
 
 <template>
-  <div class="p-6 bg-blue-500 text-white text-2xl rounded-lg">
-    Hello Tailwind + Vue 🎉
-  </div>
+  <LoginForm />
 </template>
 
 <style scoped></style>
