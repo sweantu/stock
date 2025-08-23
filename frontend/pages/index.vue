997ff7c2dfc4f7f5d322dc64f30b1af4097e7a2e
Anh Tu <sweantu@gmail.com>
2025-08-23 12:29:47 +0700

    frontend: config pinia and nuxt ui

---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useCounterStore } from '~/stores/counter'
+
+const counter = useCounterStore()
+</script>
+
+<template>
+    <UContainer>
+        <UCard>
+            <UButton @click="counter.increment">
+                Count: {{ counter.count }}
+            </UButton>
+        </UCard>
+    </UContainer>
+</template>
